6d1cafece18cb7804e283b0007be4ac7c275c232
interwap <muyexzy4shizzle@yahoo.com>
2017-01-26 12:05:19 +0000

    v1.0

---
+++ README.md
@@ -0,0 +1,47 @@
+# fingerprint-fp07
+A nifty (Android) library for verification and authentication of fingerprint on the fgtit-fp07 mobile device
+Official release library for fgtit-fp07 device.
+
+Usage:
+
+Step 1. Add the JitPack repository to your build file
+Add it in your root build.gradle at the end of repositories:
+
+```gradle
+
+    allprojects {
+    		repositories {
+    			...
+    			maven { url 'https://jitpack.io' }
+    		}
+    	}
+
+ ```
+
+Step 2. Add the dependency
+```gradle
+    dependencies {
+    	        compile 'com.github.interwap:fingerprint-fp07:v1.0'
+    	}
+ ```
+
+Get more information about versions and releases here: [`jitpack`](https://jitpack.io/#interwap/fingerprint-fp07/v1.0)
+
+
+
+License
+=======
+
+    Copyright 2017 Ikomi Moses
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/com/praescient/components/praescientlibrary/MainActivity.java
@@ -0,0 +1,109 @@
+package com.praescient.components.praescientlibrary;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.praescient.components.enrolment_fp07.EnrolFP07;
+import com.praescient.components.fingerprint_fp07.Fp07;
+
+public class MainActivity extends AppCompatActivity implements Fp07.MatchListener {
+
+    private Context context = MainActivity.this;
+    private android.app.FragmentManager fragmentManager;
+    private Fp07 popup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fragmentManager = getFragmentManager();
+
+
+        final Display display = getWindowManager().getDefaultDisplay();
+
+        final String template = "AwFmIY4A+f6ABoACgAKAAoACgAKAAoACgAKAAoACgAKAAoACgAKAAgAAAAAAAAAAAAAAAAAAAAA1\n" +
+                "i9NeX4wQXmkQq94Wl1beNBlUPkQkUv5frOi+PDDT3iWxqt4ss6reM7iUfiM7Kv5KO5NeFLyV/j2+\n" +
+                "6Z4bv2r+GYiX/z4KJ78fC5ZfPBHonysiVP86qamfZB5nPDujE3wypSocPy0T3GXD57xqHtH9K7xU\n" +
+                "fWS7ElprvJDybEDROmc+53IAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n" +
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMBZiGMAP/+gAKAAoACgAKAAoACgAKAAoACgAKA\n" +
+                "AoACgAKAAoACgAIAAAAAAAAAAAAAAAAAAAAATpQoficZVv5EmxP+bS+o3kA6FJ4vvCt+IL2WnlW9\n" +
+                "095vvpL+KEBrPkjAqh4XC8H/b4yQNxsgrH86pBS/VSTS30mraf8xiNkUSyUTfEInKhxJstScM7Mr\n" +
+                "PCkKmJ1Fi9KdTYvn/S+Nlp06NGtdOD2UvVacEzJSnSmyTS9UWhMwAtIYMRfTAAAAAAAAAAAAAAAA\n" +
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                popup = Fp07.newInstance(context, "FingerprintFPO5 Biometrics");
+
+                popup.setTextSize(20);
+                popup.show(fragmentManager, "biometric_fragment");
+
+
+                    popup.match(template);
+
+
+
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                       // .setAction("Action", null).show();
+
+                //Toast.makeText(context, "Display: " + display.getWidth() + " " + display.getHeight(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public void isMatch(boolean match) {
+
+        popup.dismiss();
+
+        if(match){
+            Toast.makeText(context, "correct", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(context, "incorrect", Toast.LENGTH_LONG).show();
+        }
+    }
+}
